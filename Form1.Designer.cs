a010d6be9d0db450e90b9bab3122aa3f95faea3f
sandinjohnruz-prog <sandingjohnruz@gmail.com>
2026-04-24 17:03:10 +0800

    Initial commit - POS left and middle panel

---
+++ Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Sanding_Win_Form_POS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.Catalog = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtItemCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Code = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ItemName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Stock = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxTQty = new System.Windows.Forms.TextBox();
+            this.BtnAddOrder = new System.Windows.Forms.Button();
+            this.BtnRmvOrder = new System.Windows.Forms.Button();
+            this.btnClrAll = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.TxtItemCode);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Catalog);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.TxtSearch);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(380, 640);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(807, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(379, 640);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.listView1);
+            this.panel3.Controls.Add(this.btnClrAll);
+            this.panel3.Controls.Add(this.BtnRmvOrder);
+            this.panel3.Controls.Add(this.BtnAddOrder);
+            this.panel3.Controls.Add(this.TxTQty);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(376, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(425, 640);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ITEM CATALOG";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtSearch
+            // 
+            this.TxtSearch.Location = new System.Drawing.Point(70, 52);
+            this.TxtSearch.Name = "TxtSearch";
+            this.TxtSearch.Size = new System.Drawing.Size(200, 20);
+            this.TxtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(276, 49);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(85, 25);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // Catalog
+            // 
+            this.Catalog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Code,
+            this.ItemName,
+            this.Price,
+            this.Stock});
+            this.Catalog.FullRowSelect = true;
+            this.Catalog.HideSelection = false;
+            this.Catalog.Location = new System.Drawing.Point(10, 90);
+            this.Catalog.Name = "Catalog";
+            this.Catalog.Size = new System.Drawing.Size(355, 480);
+            this.Catalog.TabIndex = 4;
+            this.Catalog.UseCompatibleStateImageBehavior = false;
+            this.Catalog.View = System.Windows.Forms.View.Details;
+            this.Catalog.SelectedIndexChanged += new System.EventHandler(this.Catalog_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 585);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Code";
+            // 
+            // TxtItemCode
+            // 
+            this.TxtItemCode.Location = new System.Drawing.Point(60, 582);
+            this.TxtItemCode.Name = "TxtItemCode";
+            this.TxtItemCode.Size = new System.Drawing.Size(120, 20);
+            this.TxtItemCode.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(200, 585);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Price(PHP)";
+            // 
+            // Code
+            // 
+            this.Code.Text = "Code";
+            // 
+            // ItemName
+            // 
+            this.ItemName.Text = "Item Name";
+            this.ItemName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ItemName.Width = 135;
+            // 
+            // Price
+            // 
+            this.Price.Text = "Price";
+            this.Price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Stock
+            // 
+            this.Stock.Text = "Stock";
+            this.Stock.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Stock.Width = 100;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(27, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(380, 30);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Current Order";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(10, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "QTY";
+            // 
+            // TxTQty
+            // 
+            this.TxTQty.Location = new System.Drawing.Point(50, 52);
+            this.TxTQty.Name = "TxTQty";
+            this.TxTQty.Size = new System.Drawing.Size(60, 20);
+            this.TxTQty.TabIndex = 2;
+            this.TxTQty.Text = "1";
+            // 
+            // BtnAddOrder
+            // 
+            this.BtnAddOrder.Location = new System.Drawing.Point(116, 49);
+            this.BtnAddOrder.Name = "BtnAddOrder";
+            this.BtnAddOrder.Size = new System.Drawing.Size(80, 25);
+            this.BtnAddOrder.TabIndex = 3;
+            this.BtnAddOrder.Text = "ADD";
+            this.BtnAddOrder.UseVisualStyleBackColor = true;
+            this.BtnAddOrder.Click += new System.EventHandler(this.BtnAddOrder_Click);
+            // 
+            // BtnRmvOrder
+            // 
+            this.BtnRmvOrder.Location = new System.Drawing.Point(202, 49);
+            this.BtnRmvOrder.Name = "BtnRmvOrder";
+            this.BtnRmvOrder.Size = new System.Drawing.Size(80, 25);
+            this.BtnRmvOrder.TabIndex = 4;
+            this.BtnRmvOrder.Text = "REMOVE";
+            this.BtnRmvOrder.UseVisualStyleBackColor = true;
+            this.BtnRmvOrder.Click += new System.EventHandler(this.BtnRmvOrder_Click);
+            // 
+            // btnClrAll
+            // 
+            this.btnClrAll.Location = new System.Drawing.Point(288, 49);
+            this.btnClrAll.Name = "btnClrAll";
+            this.btnClrAll.Size = new System.Drawing.Size(80, 25);
+            this.btnClrAll.TabIndex = 5;
+            this.btnClrAll.Text = "CLEAR";
+            this.btnClrAll.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(30, 90);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(355, 480);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Code";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Item Name";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "QTY";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 40;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Price";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 40;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "SubTotal";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 85;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "POS Only Pos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView Catalog;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtItemCode;
+        private System.Windows.Forms.ColumnHeader Code;
+        private System.Windows.Forms.ColumnHeader ItemName;
+        private System.Windows.Forms.ColumnHeader Price;
+        private System.Windows.Forms.ColumnHeader Stock;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnAddOrder;
+        private System.Windows.Forms.TextBox TxTQty;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnRmvOrder;
+        private System.Windows.Forms.Button btnClrAll;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sanding_Win_Form_POS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAddOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRmvOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Catalog_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
